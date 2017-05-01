2848fce43e838fa35819343417b84671a3bb83c7
Nick Meyer <meyersoftware@gmail.com>
2017-05-01 16:21:03 -0400

    hey hey

--- ShoppingCartSample.sql
+++ ShoppingCartSample.sql
@@ -23,16 +23,44 @@ ALTER TABLE dbo.Customer ADD CONSTRAINT
 
 GO
 
-CREATE TABLE dbo.Product
-	(
-	ProductID int NOT NULL IDENTITY (1, 1),
-	Name nvarchar(100) NOT NULL,
-	ProductDescription text NOT NULL,
-	Price money NOT NULL,
-	Stock int NOT NULL
-	)  ON [PRIMARY]
-	 TEXTIMAGE_ON [PRIMARY]
+CREATE TABLE [dbo].[Product](
+	[ProductID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+	[ProductDescription] [text] NOT NULL,
+	[Price] [money] NOT NULL,
+	[Stock] [int] NOT NULL,
+	[SortOrder] [int] NULL,
+	[Image] [nvarchar](255) NULL,
+ CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED 
+(
+	[ProductID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
 GO
+
+CREATE TABLE dbo.Orders
+	(
+	[OrderID] int NOT NULL IDENTITY (1, 1),
+	[CustomerID] int NOT NULL,
+	[ProductID] int NOT NULL,
+
+ CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED 
+(
+	[OrderID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE dbo.Customer ADD CONSTRAINT
+	PK_Customer PRIMARY KEY CLUSTERED 
+	(
+	CustomerID
+	) WITH( STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+
+GO
+
 
 ALTER TABLE dbo.Product ADD CONSTRAINT
 	PK_Product PRIMARY KEY CLUSTERED 
@@ -42,20 +70,20 @@ ALTER TABLE dbo.Product ADD CONSTRAINT
 
 GO
 
-insert into Product (Name, ProductDescription, Stock)
-values ('thing a ma bob', 'Many people confuse this with ''thing a man bob'', but you can''t buy people.  It''s the next best thing I suppose.', 1)
+insert into Product (Name, ProductDescription,Price, Stock, SortOrder, [Image])
+values ('thing a ma bob', 'Many people confuse this with ''thing a man bob'', but you can''t buy people.  It''s the next best thing I suppose.',10.00, 1, 1, 'thingamabob.jpg')
 
-insert into Product (Name, ProductDescription, Stock)
-values ('thing a ma jig', 'War broke out centuries ago based on whether this was superior to thing a ma bob. It ended in a stalemate. Also, it''s possible it never happened.', 2)
+insert into Product (Name, ProductDescription,Price, Stock, SortOrder, [Image])
+values ('thing a ma jig', 'War broke out centuries ago based on whether this was superior to thing a ma bob. It ended in a stalemate. Also, it''s possible it never happened.',20.00, 2, 2, 'thingamajig.jpg')
 
-insert into Product (Name, ProductDescription, Stock)
-values ('a hearty jig', 'Think Ireland. Think dancing. Think feeling truly alive.', 2)
+insert into Product (Name, ProductDescription,Price , Stock, SortOrder, [Image])
+values ('a hearty jig', 'Think Ireland. Think dancing. Think feeling truly alive.', 50.00, 2, 3, 'jig.jpg')
 
-insert into Product (Name, ProductDescription, Price, Stock)
-values ('some hearty soup', 'Like mom used to make when you were faking sick from school, to make you feel guilty. Good stuff.',25.00, 4)
+insert into Product (Name, ProductDescription, Price, Stock, SortOrder, [Image])
+values ('some hearty soup', 'Like mom used to make when you were faking sick from school, to make you feel guilty. Good stuff.',25.00, 4, 4, 'heartysoup.jpg')
 
-insert into Product (Name, ProductDescription, Price, Stock)
-values ('soup stain remover', 'You ate the soup while you were getting jiggy, didn''t you? Well pay up sucker.',100000.00, 6)
+insert into Product (Name, ProductDescription, Price, Stock, SortOrder, [Image])
+values ('soup stain remover', 'You ate the soup while you were getting jiggy, didn''t you? Well pay up sucker.',100000.00, 6, 5, 'stain.jpg')
 
 GO
 
